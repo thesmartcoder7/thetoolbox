9976280f0f80bcabf336a57985c28a7ad7b0bb79
samuel martins <samuel.martins4.sm@gmail.com>
2024-02-26 22:46:22 +0300

    style: add the authentication modal

--- src/app/components/landing/landing.component.ts
+++ src/app/components/landing/landing.component.ts
@@ -9,4 +9,36 @@ import { Component } from '@angular/core';
 })
 export class LandingComponent {
   currentYear: number = new Date().getFullYear();
+  authModal: boolean = false;
+  emailInput: string = 'login-email';
+  passwordInput: string = 'login-password';
+  signUp: boolean = false;
+  buttonValue: string = 'login';
+  greeting: string = 'Welcome Back!';
+
+  toggleAuthModal() {
+    if (this.authModal) {
+      this.authModal = false;
+    } else {
+      this.authModal = true;
+    }
+  }
+
+  toggleSignUp() {
+    if (this.signUp) {
+      this.signUp = false;
+      this.buttonValue = 'Login';
+    } else {
+      this.signUp = true;
+      this.greeting = 'Hello Stranger . . . ';
+      this.emailInput = 'signup-email';
+      this.passwordInput = 'signup-password';
+      this.buttonValue = 'Sign Up';
+    }
+  }
+
+  attributeSwitch() {
+    this.emailInput = 'signup-email';
+    this.passwordInput = 'signup-password';
+  }
 }
