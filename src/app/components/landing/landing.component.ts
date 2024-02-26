718844cf5a6d50e5a52acfa92e4ea2b9b976c841
samuel martins <samuel.martins4.sm@gmail.com>
2024-02-26 23:51:27 +0300

    feat: update the sigup and login function

--- src/app/components/landing/landing.component.ts
+++ src/app/components/landing/landing.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+// import { User } from 'src/app/interfaces/user';
 
 @Component({
   selector: 'app-landing',
@@ -7,7 +8,7 @@ import { Component } from '@angular/core';
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.scss',
 })
-export class LandingComponent {
+export class LandingComponent implements OnInit {
   currentYear: number = new Date().getFullYear();
   authModal: boolean = false;
   emailInput: string = 'login-email';
@@ -16,7 +17,50 @@ export class LandingComponent {
   buttonValue: string = 'login';
   greeting: string = 'Welcome Back!';
 
-  toggleAuthModal() {
+  loginFunction: string = 'userLogin(mail.value, pass.value)';
+  signUpFunction: string = 'userSignUp(name.value, mail.value, pass.value)';
+
+  ngOnInit(): void {}
+
+  onFormSubmit(event: Event) {
+    event.preventDefault();
+    // signup variables
+    let fullName = document.getElementById('user-name') as HTMLInputElement;
+    let signUpEmail = document.getElementById(
+      'signup-email'
+    ) as HTMLInputElement;
+    let signUpPass = document.getElementById(
+      'signup-password'
+    ) as HTMLInputElement;
+
+    let loginEmail = document.getElementById('login-email') as HTMLInputElement;
+    let loginPassword = document.getElementById(
+      'login-password'
+    ) as HTMLInputElement;
+
+    if (fullName && signUpEmail && signUpPass) {
+      this.userSignUp(fullName.value, signUpEmail.value, signUpPass.value);
+    } else {
+      this.userLogin(loginEmail.value, loginPassword.value);
+    }
+  }
+
+  userLogin(email: string, password: string): void {
+    // console.log({
+    //   email: email,
+    //   password: password,
+    // });
+  }
+
+  userSignUp(name: string, email: string, password: string): void {
+    // console.log({
+    //   'full name': name,
+    //   email: email,
+    //   password: password,
+    // });
+  }
+
+  toggleAuthModal(): void {
     if (this.authModal) {
       this.authModal = false;
     } else {
@@ -24,7 +68,7 @@ export class LandingComponent {
     }
   }
 
-  toggleSignUp() {
+  toggleSignUp(): void {
     if (this.signUp) {
       this.signUp = false;
       this.buttonValue = 'Login';
@@ -37,7 +81,7 @@ export class LandingComponent {
     }
   }
 
-  attributeSwitch() {
+  attributeSwitch(): void {
     this.emailInput = 'signup-email';
     this.passwordInput = 'signup-password';
   }
