0af28cfface7ca796218584f5341c534ab5be844
Samuel Martins <samuel.martins4.sm@gmail.com>
2025-03-06 13:45:13 +0300

    style: update responsiveness to only focus on tablet-pros and above

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'TheToolBox';
+  isTooSmall = false;
+
+  constructor(private breakpointObserver: BreakpointObserver) {
+    this.breakpointObserver.observe([Breakpoints.Small, Breakpoints.Handset]).subscribe(result => {
+      this.isTooSmall = result.matches;
+    });
+  }
 }
